e622cde01bf3186eb3b39996bbb9ec7eec2bf375
Prathmesh-rajurkar <prathmeshrajurkar199@gmail.com>
2024-03-11 22:45:57 +0530

    added basic functionalities

--- App/views.py
+++ App/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def CreateRoom(request):
+    return render(request, 'index.html')
+
+def MessageView(request,room_name,username):
+    return render(request, '_message.html')
--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'daphne',
+    'App',
+    # 'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'ChatApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
