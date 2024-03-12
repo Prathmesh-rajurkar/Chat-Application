b6cc264096fd581e52bd2070206eba401e1d5fb1
Prathmesh-rajurkar <prathmeshrajurkar199@gmail.com>
2024-03-12 23:18:53 +0530

    Added CSS

--- App/admin.py
+++ App/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Room)
-admin.site.register(Message)
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['room', 'sender', 'message']
+
+admin.site.register(Message, MessageAdmin)
